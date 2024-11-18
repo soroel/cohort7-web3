da3da38464c393ebcf96e2e43772eabd3395382f
soroel <sorophinenoel@gmail.com>
2024-11-18 21:26:28 +0300

    bestseller

--- cohort7we/contracts/Todo.sol
+++ cohort7we/contracts/Todo.sol
@@ -22,12 +22,6 @@ pragma solidity ^0.8.2;
 // inheritance -
 
 
-
-
-
-
-
-
 contract BookStore {
 
     address public owner;
@@ -85,5 +79,48 @@ contract BookStore {
         payable(owner).transfer(msg.value);
         emit BookPurchased(_bookId, msg.sender, _quantity);
     }
+
+}
+
+contract AdvancedBookStore is BookStore {
+    mapping(uint256 => bool) public bestsellers;
+
+    event BookRemoved(uint256 indexed  bookId);
+    event BookMarkedAsBestseller(uint256 indexed  bookId);
+
+    constructor(address _owner) BookStore(_owner) {}
+
+
+    // mark book as a bestseller
+    function markAsBestseller(uint256 _bookId) public onlyOwner {
+        require(books[_bookId].price != 0, "Book does not exist.");
+        bestsellers[_bookId] = true;
+        emit BookMarkedAsBestseller(_bookId);
+    }
+// isAdmin, isAuth, isUser - true or false
+    function isBestseller(uint256 _bookId) public view returns (bool) {
+        return bestsellers[_bookId];
+    }
+
+    // Function to remove a book from the store
+    function removeBook(uint256 _bookId) public onlyOwner {
+        require(books[_bookId].price != 0, "Book does not exist.");
+        delete books[_bookId];
+
+        for (uint256 i = 0; i < bookIds.length; i++) {
+            if (bookIds[i] == _bookId) {
+                bookIds[i] = bookIds[bookIds.length - 1];
+                bookIds.pop(); // array remove
+                break;
+            }
+        }
+
+        if (bestsellers[_bookId]) {
+            delete bestsellers[_bookId]; // Remove from bestsellers if applicable
+        }
+
+        emit BookRemoved(_bookId);
+    }
+
 
 }
